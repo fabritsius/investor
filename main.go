eaeb5b546f8a63384cb90ac2d6350221820f5758
fabritsius <antonfabritsius@gmail.com>
2021-01-08 23:03:24 +0300

    Store separate data for each stock

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"errors"
 	"flag"
 	"fmt"
 	"log"
@@ -37,90 +36,108 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	portfolioStatsByCurrency := getTotalPositionsValue(positions)
-	portfolioStats := convertPortfolioStatsToDollar(client, portfolioStatsByCurrency)
-	fmt.Println(portfolioStats)
+	portfolioStats := getPortfolioStats(client, positions)
+	fmt.Println(*portfolioStats)
 }
 
-func getTotalPositionsValue(positions []sdk.PositionBalance) map[sdk.Currency]*PortfolioStats {
-	portfolioStats := make(map[sdk.Currency]*PortfolioStats)
+func getPortfolioStats(client *sdk.RestClient, positions []sdk.PositionBalance) *PortfolioStats {
+	portfolioStats := &PortfolioStats{
+		Currency: "USD",
+		Date:     time.Now(),
+		Stocks:   []StockData{},
+		Totals:   PortfolioTotals{},
+	}
+	dollarConversionMap := make(DollarConversionMap)
+	dollarRubPrice, err := getDollarPriceInRubles(client)
+	if err == nil {
+		dollarConversionMap["RUB"] = dollarRubPrice
+	}
+
 	for _, pos := range positions {
 		currency := pos.AveragePositionPrice.Currency
 
-		positionStats := &PortfolioStats{
-			Date:     time.Now(),
-			Invested: pos.AveragePositionPrice.Value * pos.Balance,
-			Yield:    pos.ExpectedYield.Value,
-			Stocks:   make(map[string]float64),
-			Currency: currency,
+		price, err := convertToDollar(currency, pos.AveragePositionPrice.Value, dollarConversionMap)
+		if err != nil {
+			log.Fatalln(err)
 		}
-		positionStats.Stocks[pos.Name] = pos.Balance
+		yield, err := convertToDollar(currency, pos.ExpectedYield.Value, dollarConversionMap)
+		if err != nil {
+			log.Fatalln(err)
+		}
 
-		if prevStats, ok := portfolioStats[currency]; ok {
-			if err := (*prevStats).Add(positionStats, 1); err != nil {
-				log.Fatalln(err)
-			}
-		} else {
-			portfolioStats[currency] = positionStats
+		stockIDs := map[string]string{
+			"FIGI": pos.FIGI,
+			"ISIN": pos.ISIN,
 		}
+		stockData := StockData{
+			IDs:     stockIDs,
+			Name:    pos.Name,
+			Balance: pos.Balance,
+			Price:   price,
+			Yield:   yield,
+		}
+
+		portfolioStats.Totals.Invested += stockData.Balance * stockData.Price
+		portfolioStats.Totals.Yield += stockData.Yield
+		portfolioStats.Stocks = append(portfolioStats.Stocks, stockData)
 	}
 	return portfolioStats
 }
 
-func convertPortfolioStatsToDollar(client *sdk.RestClient, portfolioStatsByCurrency map[sdk.Currency]*PortfolioStats) PortfolioStats {
-	totalPortfolioStats := portfolioStatsByCurrency["USD"]
-	for currency, portfolioStats := range portfolioStatsByCurrency {
-		switch currency {
-		case "RUB":
-			dollarPrice := getDollarPrice(client)
-			if err := totalPortfolioStats.Add(portfolioStats, 1/dollarPrice); err != nil {
-				log.Fatalln(err)
-			}
-		}
+func convertToDollar(currency sdk.Currency, value float64, dollarConversionMap DollarConversionMap) (float64, error) {
+	switch currency {
+	case "USD":
+		return value, nil
+	case "RUB":
+		dollarPrice := dollarConversionMap["RUB"]
+		return value / dollarPrice, nil
+	default:
+		return 0, fmt.Errorf("cannot convert: unrecognised currency %q", currency)
 	}
-	return *totalPortfolioStats
 }
 
 // PortfolioStats contains main portfolio stats for the moment
 type PortfolioStats struct {
+	Currency sdk.Currency
 	Date     time.Time
+	Stocks   []StockData
+	Totals   PortfolioTotals
+}
+
+// PortfolioTotals contains calculated totals for portfolio
+type PortfolioTotals struct {
 	Invested float64
 	Yield    float64
-	Stocks   map[string]float64
-	Currency sdk.Currency
 }
 
-// Add function adds two PortfolioStats given a multiplier which can be 1
-func (s *PortfolioStats) Add(new *PortfolioStats, multiplier float64) error {
-	if s.Currency != new.Currency && multiplier == 0 {
-		return errors.New("Can't add. Currencies do no match")
-	}
-
-	s.Invested += new.Invested * multiplier
-	s.Yield += new.Yield * multiplier
-	for bk, bv := range new.Stocks {
-		s.Stocks[bk] = bv
-	}
-
-	return nil
+// StockData contains main data about the stock
+type StockData struct {
+	IDs     map[string]string
+	Name    string
+	Balance float64
+	Price   float64
+	Yield   float64
 }
+
+// DollarConversionMap has conversion scalars for currencies
+type DollarConversionMap = map[sdk.Currency]float64
 
 // String method prints PortfolioStats in a table-like form
 func (s PortfolioStats) String() string {
 	stocks, _ := json.MarshalIndent(s.Stocks, "", " ")
-	return fmt.Sprintf("%3s: invested: %10.2f | yield: %10.2f | total: %10.2f\n%s", s.Currency, s.Invested, s.Yield, s.Invested+s.Yield, string(stocks))
+	return fmt.Sprintf("%s\n\nTotals in %3s >> invested: %10.2f | yield: %10.2f | grand total: %10.2f", string(stocks), s.Currency, s.Totals.Invested, s.Totals.Yield, s.Totals.Invested+s.Totals.Yield)
 }
 
-func getDollarPrice(client *sdk.RestClient) float64 {
+func getDollarPriceInRubles(client *sdk.RestClient) (float64, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	dollarFIGI := "BBG0013HGFT4"
 	candles, err := client.Candles(ctx, time.Now().AddDate(0, 0, -7), time.Now(), sdk.CandleInterval1Day, dollarFIGI)
 	if err != nil {
-		log.Fatalln(err)
+		return 0, err
 	}
 	latestCandle := candles[len(candles)-1]
 	dollarPrice := latestCandle.ClosePrice
-	return dollarPrice
+	return dollarPrice, nil
 }
