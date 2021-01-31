2e4e472b87d54c6a881679760ac59d2bc5ba2104
fabritsius <antonfabritsius@gmail.com>
2021-01-31 16:20:35 +0300

    Use Cassandra to store user accounts

--- aggregator/go.mod
+++ aggregator/go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/fabritsius/envar v1.0.1
 	github.com/fabritsius/investor/messages v0.0.0-20210128174819-fd4734fa4c44
+	github.com/gocql/gocql v0.0.0-20210129204804-4364a4b9cfdd
 	golang.org/x/net v0.0.0-20210119194325-5f4716e94777
 	google.golang.org/grpc v1.35.0
 )
--- aggregator/main.go
+++ aggregator/main.go
@@ -4,9 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/fabritsius/envar"
 	"github.com/fabritsius/investor/messages"
+	"github.com/gocql/gocql"
 
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
@@ -26,19 +28,77 @@ func main() {
 	var conn *grpc.ClientConn
 	conn, err := grpc.Dial(fmt.Sprintf(":%s", cfg.Port), grpc.WithInsecure())
 	if err != nil {
-		log.Fatalf("did not connect: %s", err)
+		log.Fatalf("did not connect to grpc: %s", err)
 	}
 	defer conn.Close()
 
+	dbSession, err := getDbSession()
+	if err != nil {
+		log.Fatalf("did not connect to the database: %s", err)
+	}
+	defer dbSession.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	for account := range getUserAccounts(ctx, dbSession) {
+		log.Println("Account:", account.userID, account.accountType, account.token)
+
+		if account.accountType == "tinkoff" {
+			portfolio, err := GetTinkoffPortfolio(conn, account.token)
+			if err != nil {
+				log.Fatalf("error when calling getTinkoffPortfolio: %s", err)
+			}
+
+			stats, _ := json.MarshalIndent(portfolio, "", " ")
+			log.Printf("Portfolio: %s", string(stats))
+		}
+	}
+
+}
+
+// GetTinkoffPortfolio takes grpc connection and Tinkoff API Token
+// and returns user's Tinkoff portfolio with stocks and calculated totals
+func GetTinkoffPortfolio(conn *grpc.ClientConn, tinkoffToken string) (*messages.PortfolioReply, error) {
 	client := messages.NewPortfolioClient(conn)
 
 	response, err := client.GetPortfolio(context.Background(), &messages.PortfolioRequest{
-		Options: map[string]string{"token": cfg.TinkoffToken},
+		Options: map[string]string{"token": tinkoffToken},
 	})
 	if err != nil {
-		log.Fatalf("error when calling GetPortfolio: %s", err)
+		return nil, err
 	}
 
-	stats, _ := json.MarshalIndent(response, "", " ")
-	log.Printf("Portfolio: %s", string(stats))
+	return response, nil
+}
+
+func getDbSession() (*gocql.Session, error) {
+	cluster := gocql.NewCluster("127.0.0.1")
+	cluster.Keyspace = "investor"
+	return cluster.CreateSession()
+}
+
+func getUserAccounts(ctx context.Context, dbSession *gocql.Session) <-chan *UserAccount {
+	query := "SELECT * FROM accounts_by_user"
+	scanner := dbSession.Query(query).WithContext(ctx).Iter().Scanner()
+	result := make(chan *UserAccount)
+	go func() {
+		defer close(result)
+		for scanner.Next() {
+			account := &UserAccount{}
+			if err := scanner.Scan(&account.userID, &account.accountType, &account.token); err != nil {
+				log.Fatalf("gocql scanner error: %s", err)
+				continue
+			}
+			result <- account
+		}
+	}()
+	return result
+}
+
+// UserAccount represents one of user's stocks account
+type UserAccount struct {
+	userID      gocql.UUID
+	accountType string
+	token       string
 }
