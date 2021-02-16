a6bb1e77d06aa700e3b84cf176801ef36644e07b
fabritsius <antonfabritsius@gmail.com>
2021-02-16 21:57:47 +0300

    Add a ticker and update stats with an interval

--- aggregator/go.mod
+++ aggregator/go.mod
@@ -3,7 +3,7 @@ module github.com/fabritsius/investor/aggregator
 go 1.16
 
 require (
-	github.com/fabritsius/envar v1.0.1
+	github.com/fabritsius/envar v1.1.0
 	github.com/fabritsius/investor/messages v0.0.0-20210128174819-fd4734fa4c44
 	github.com/gocql/gocql v0.0.0-20210129204804-4364a4b9cfdd
 	golang.org/x/net v0.0.0-20210119194325-5f4716e94777
--- aggregator/main.go
+++ aggregator/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"time"
@@ -15,7 +14,8 @@ import (
 )
 
 type config struct {
-	Port string `env:"TINKOFF_PORT"`
+	Port           string `env:"TINKOFF_PORT"`
+	TickPeriodMins int    `env:"AGGR_TICK_PERIOD" default:"10"`
 }
 
 func main() {
@@ -24,12 +24,12 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	var conn *grpc.ClientConn
-	conn, err := grpc.Dial(fmt.Sprintf(":%s", cfg.Port), grpc.WithInsecure())
+	var tinkoffConn *grpc.ClientConn
+	tinkoffConn, err := grpc.Dial(fmt.Sprintf(":%s", cfg.Port), grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("did not connect to grpc: %s", err)
 	}
-	defer conn.Close()
+	defer tinkoffConn.Close()
 
 	var db *models.DB
 	if db, err = models.Connect("127.0.0.1"); err != nil {
@@ -40,22 +40,32 @@ func main() {
 	}
 	defer db.Close()
 
+	log.Printf("start: update stats every %d minutes", cfg.TickPeriodMins)
+	updatePortfolioStats(db, tinkoffConn)
+	for range time.Tick(time.Duration(cfg.TickPeriodMins) * time.Minute) {
+		log.Println("tick: updating portfolio stats")
+		updatePortfolioStats(db, tinkoffConn)
+	}
+}
+
+func updatePortfolioStats(db *models.DB, tinkoffConn *grpc.ClientConn) {
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 	defer cancel()
 
 	for item := range db.GetAllUserAccounts(ctx) {
 		if item.Error != nil {
-			log.Printf("account error: %s", err)
+			log.Printf("account error: %s", item.Error)
 			continue
 		}
 
 		account := item.Account
-		log.Println("Account:", account.UserID, account.AccountType, account.Token)
+		log.Println("account:", account.UserID, account.AccountType)
 
 		var portfolio *messages.PortfolioReply
+		var err error
 		switch account.AccountType {
 		case "tinkoff":
-			if portfolio, err = GetTinkoffPortfolio(conn, account.Token); err != nil {
+			if portfolio, err = GetTinkoffPortfolio(tinkoffConn, account.Token); err != nil {
 				log.Printf("error when calling getTinkoffPortfolio: %s", err)
 			}
 		}
@@ -74,11 +84,12 @@ func main() {
 				log.Printf("failed to update portfolio stats: %s", err)
 			}
 
-			stats, _ := json.MarshalIndent(portfolio, "", " ")
-			log.Printf("Portfolio: %s", string(stats))
+			investedValue := portfolio.Data.Totals.Invested
+			yieldValue := portfolio.Data.Totals.Yield
+			totalValue := investedValue + yieldValue
+			log.Printf("portfolio totals: %s: $%.2f + $%.2f = $%.2f", account.AccountType, investedValue, yieldValue, totalValue)
 		}
 	}
-
 }
 
 // GetTinkoffPortfolio takes grpc connection and Tinkoff API Token
